6d9e34f10222c0de1626efe6c0acaea73fa5edaf
@meran <amr.bin.an@gmail.com>
2020-04-23 15:43:34 +0700

    #4 Membuat Project React dengan create react app ReactJS

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const HelloWorld = () => {
+  return <p>Hello world</p>
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
