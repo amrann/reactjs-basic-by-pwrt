5dbc37c9e8f5e4e3d8c74127e80638c94220a5cc
@meran <amr.bin.an@gmail.com>
2020-04-24 15:33:24 +0700

    #7 Update Parent oleh child component

--- src/kontainer/Beranda.jsx
+++ src/kontainer/Beranda.jsx
@@ -1,22 +1,38 @@
 import React, {Component} from 'react';
 
 // Video #6
-import YutupKomponen from '../komponen/YutupKomponen';
+// import YutupKomponen from '../komponen/YutupKomponen';
+// class Beranda extends Component {
+//     render(){
+//         return(
+//             <div>
+//                 <p>Yutup Komponen</p>
+//                 <hr/>
+//                 <YutupKomponen waktu="8.02" judul="Judul ke-1" deskripsi="Deskripsi judul ke-1"/>
+//                 <YutupKomponen waktu="7.12" judul="Judul ke-2" deskripsi="Deskripsi judul ke-2"/>
+//                 <YutupKomponen waktu="4.42" judul="Judul ke-3" deskripsi="Deskripsi judul ke-3"/>
+//                 <YutupKomponen waktu="2.12" judul="Judul ke-4" deskripsi="Deskripsi judul ke-4"/>
+//                 <YutupKomponen/>
+//             </div>
+//         ) 
+//     }
+// }
+// export default Beranda;
+// end-Video #6
 
+// Video #7
+import YutupKomponen from '../komponen/YutupKomponen';
+import Produk from './Produk';
 class Beranda extends Component {
     render(){
         return(
             <div>
-                <p>Yutup Komponen</p>
+                <p>Counter</p>
                 <hr/>
-                <YutupKomponen waktu="8.02" judul="Judul ke-1" deskripsi="Deskripsi judul ke-1"/>
-                <YutupKomponen waktu="7.12" judul="Judul ke-2" deskripsi="Deskripsi judul ke-2"/>
-                <YutupKomponen waktu="4.42" judul="Judul ke-3" deskripsi="Deskripsi judul ke-3"/>
-                <YutupKomponen waktu="2.12" judul="Judul ke-4" deskripsi="Deskripsi judul ke-4"/>
-                <YutupKomponen/>
+                <Produk/>
             </div>
         ) 
     }
 }
 export default Beranda;
-// end-Video #6
+// end-Video #7
